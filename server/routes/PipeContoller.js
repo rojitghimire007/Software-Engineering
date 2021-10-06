6c3e16b99bbcc637688d825dbaabcb61fbce8c3f
rojitghimire007 <rojitghimire12@gmail.com>
2021-10-05 20:16:55 -0500

    Allpipes requirement met

--- server/routes/PipeContoller.js
+++ server/routes/PipeContoller.js
@@ -35,7 +35,8 @@ const addPipe = (req, res, next) => {
   );
   client
     .query({
-      text: 'INSERT INTO pipes(location, coil_number, heat_number, grade_type, pipe_length,coating_type, coating_color, type_name, porder_id, smart_label, comments, void, schedule_class, diameter) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)',
+      text:
+        'INSERT INTO pipes(location, coil_number, heat_number, grade_type, pipe_length,coating_type, coating_color, type_name, porder_id, smart_label, comments, void, schedule_class, diameter) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)',
       values: [
         location,
         coil_no,
@@ -67,7 +68,9 @@ const addPipe = (req, res, next) => {
 
 const allPipes = (req, res, next) => {
   client
-    .query('SELECT pipe_id as id, * FROM pipes')
+    .query(
+      "SELECT void, DATE(inventory_date), CONCAT(first_name, ' ', last_name) AS inspector, location, pipe_id as id,  coil_number as coil_no, heat_number as heat_no, diameter, designation as schedule, wall_thickness, grade, pipe_length as length, pipes.coating_type as coating, color as coating_color, mfg as manufacturer, material as material_type, purchase_order as po_number, comments FROM pipes INNER JOIN schedule_and_class ON pipes.schedule_class = schedule_and_class.id INNER JOIN users ON pipes.inspector_id = users.id INNER JOIN pipe_coating ON pipes.coating_type = pipe_coating.coating_type;"
+    )
     .then((response) => {
       return res.status(200).send({ success: true, pipes: response.rows });
     })
@@ -78,3 +81,5 @@ const allPipes = (req, res, next) => {
 };
 
 module.exports = { addPipe, allPipes };
+
+// .query("SELECT pipe_id as id, * FROM pipes")
