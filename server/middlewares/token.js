9d442fa74fd3c851ae026f276023b527c1f87a07
Ashish Dev <ashishdevofficial@outlook.com>
2021-10-07 09:37:58 -0500

    Token middleware added (not implemented). Managed other middlewares

---
+++ server/middlewares/token.js
@@ -0,0 +1,27 @@
+const validateToken = require('../utils/TokenValidator');
+
+/**
+ * Extract email from the jwt token and put it in body
+ * @param {*} app -Express App
+ */
+const TokenMiddleWare = (app) => {
+  app.use(async (req, res, next) => {
+    var { token } = req.headers;
+
+    if (!token || token == 'null') req.userEmail = null;
+    else {
+      let verification = await validateToken({ token });
+
+      if (verification.success) {
+        req.userEmail = verification.decoded.email;
+      } else {
+        req.userEmail = null;
+        next('Token not verified');
+      }
+    }
+
+    next();
+  });
+};
+
+module.exports = TokenMiddleWare;
