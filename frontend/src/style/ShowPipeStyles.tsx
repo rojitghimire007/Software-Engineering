f001641fd2b0017ee84e090c246a910aa1a28d8c
cjcolemanx <cjcolemanx@gmail.com>
2021-10-18 02:50:35 -0500

    Adds further styling to actions in tables

--- frontend/src/style/ShowPipeStyles.tsx
+++ frontend/src/style/ShowPipeStyles.tsx
@@ -4,6 +4,8 @@ import { ImportantDevices } from '@mui/icons-material';
 import ColorScheme from './ColorScheme';
 import HueSelector from './HueSelector';
 
+import DragHandleIcon from '@mui/icons-material/DragHandle';
+
 const useStyles = makeStyles((theme) => ({
   header: {
     backgroundColor: ColorScheme.secondaryLight,
@@ -58,43 +60,83 @@ const useStyles = makeStyles((theme) => ({
   },
   filterRow: {},
   stickyActions: {
+    
     tfoot: {
-      border: '0px !important',
-      borderColor: '0 !important',
-      backgroundColor: 'blue !important',
+      // border: '0px !important',
+      // borderColor: '0 !important',
+      // backgroundColor: 'blue !important',
     },
     '& table:first-child': {
+      // '& tbody:first-child': {
+      //   '& tr:first-child': {
+      //     '& td': {
+      //       position: 'sticky',
+      //       left: 0,
+      //       zIndex: 999,
+      //       padding: 0 + ' !important',
+      //       backgroundColor: '#fffff5 !important',
+      //       border: '1px solid black !important',
+      //       boxShadow: '-1px 1px black',
+      //     },
+      //   },
+      // },
       '& tr': {
-        '& td:first-child, th:first-child': {
-          backgroundColor: '#fffff0',
-          
+        '& td:first-child': {
           position: 'sticky',
           left: 0,
           zIndex: 999,
+          padding: 0 + ' !important',
+          '& div': {
+            backgroundColor: '#fffff5',
+            border: '1px solid black !important',
+            boxShadow: '-1px 1px black',
+          },
         },
         '& th:first-child': {
-          
+          // backgroundColor: `${ColorScheme.primaryDark} !important`,
+          backgroundColor: '#fffff5',
+          // color: `${ColorScheme.onPrimary}`,
+          border: '1px solid black !important',  
+          position: 'sticky',
+          left: 0,
           zIndex: 999,
+          padding: 0 + ' !important',
+          // display: 'block',
+          // width: '50% !important',
+          // height: '50% !important',
+          textAlign: 'center !important',
+          
+          '& span': {
+            padding: '25% 25% !important',
+            margin: '0 !important',
+            boxSizing: 'border-box',
+            width: '100%',
+            border: '1px solid black !important',            
+            boxShadow: '1px 1px black !important',
+            position: 'relative',
+          },
+
         },
       },
       '& tr:last-child': {
-        '& td:first-child, th:first-child': {
-          backgroundColor: '#fffff0',
-          boxShadow: '10px 0px black',
-          position: 'sticky',
-          left: 0,
-          zIndex: 999,
-        },
+        // '& td:first-child, th:first-child': {
+        //   backgroundColor: '#fffff0',
+        //   boxShadow: '10px 0px black',
+        //   position: 'sticky',
+        //   left: 0,
+        //   zIndex: 999,
+        // },
       },
     },
     '& table:not(first-child)': {
       '& td, th': {
-        border: '1px solid black',
+        // border: '1px solid black',
       },
     },
     '& th':{
       '& span.material-icons.MuiIcon-root': {
-          opacity: 0.0 + '!important',
+        opacity: 0.0 + '!important',
+        backgroundImage: `url(${DragHandleIcon})`,
       },
     },
   },
