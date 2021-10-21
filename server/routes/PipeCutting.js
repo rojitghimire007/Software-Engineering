937392f9265ff2451478d762c4cd14ec626d3383
rojitghimire007 <rojitghimire12@gmail.com>
2021-10-20 23:34:38 -0500

    getCuttingElibilepipes added in PipeCutting js

--- server/routes/PipeCutting.js
+++ server/routes/PipeCutting.js
@@ -49,27 +49,6 @@ const cutPipe = async (req, res, next) => {
   }
 };
 
-/*const getCuttingEligiblePipes = async (req, res, next) => {
-  const { pipe, cut_length } = req.body;
-
-  try {
-    let eligiblePipes = await client.query(
-      `SELECT * FROM pipes WHERE (pipe = ${pipe_id} AND isCut = '${false}`
-    );
-
-    eligiblepipes = eligiblePipes.rows[0].id;
-
-    let cuttablePipes = await client.query(
-      `SELECT * FROM pipes WHERE pipe LIKE '${pipe_id}%' ORDER BY ${pipe_id} DESC 1`
-    );
-
-    cuttablePipes = cuttablePipes.rows[0].id;
-    res.status(200).send({ success: true, cuttablePipes });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-};*/
 const getCuttingEligiblePipes = async (req, res, next) => {
   try {
     let ans = await client.query(
