d75d26aa787c5b235b0aa6e00f6e0cbacf957a4d
Ashish Dev <ashishdevofficial@outlook.com>
2021-11-05 20:49:09 -0500

    edit pipe feature works

--- server/sql_queries/pipeQueries.js
+++ server/sql_queries/pipeQueries.js
@@ -10,6 +10,10 @@ const pipeQueries = {
     'INSERT INTO pipe(id, pipeSharedId, plength, updatedBy, plocation, coilNumber, comment, isVoid, isUsed, parent) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
   allPipes:
     'select id, diameter, schedule, grade, isvoid as void, updatedon as date, updatedby as inspector, plocation as location, coilnumber as coil_no, heatnumber as heat_no, thickness as wall_thickness, plength as length, coat as coating, color as coating_color, manufacture as manufacturer, materialtype as material_type, ponumber as po_number, comment as comments from pipe join pipesharedinfo using (pipeSharedId) join pipeheat using (pipeheatid) join piperef using (piperefid) join pipecoat using(coat);',
+  updatePipe:
+    'UPDATE pipe SET id = $1, plength = $2, updatedBy = $3, plocation = $4, coilNumber = $5, comment = $6, isVoid = $7, updatedon = $8 WHERE id = $9',
+  updatePipeSharedInfo:
+    'UPDATE pipesharedinfo SET coat = $1, grade = $2, ponumber = $3, materialtype = $4 where pipesharedid = $5',
 };
 
 module.exports = pipeQueries;
