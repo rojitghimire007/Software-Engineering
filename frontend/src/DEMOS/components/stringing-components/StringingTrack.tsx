14a147aee38e31cca741e11e596918e0624cf01f
cjcolemanx <cjcolemanx@gmail.com>
2021-11-15 19:22:34 -0600

    did something

--- frontend/src/DEMOS/components/stringing-components/StringingTrack.tsx
+++ frontend/src/DEMOS/components/stringing-components/StringingTrack.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const StringingTrack = ({name}: any) => {
+const StringingTrack = ({ name }: any) => {
     return (
-        <div style={{textTransform: 'uppercase', backgroundColor: 'rgba(200,200,200,1)', width: '100%', height: '200px'}}>
-            {name} Track Item Here
-        </div>
+        <>
+            {name != '' ?
+                <div style={{ textTransform: 'uppercase', backgroundColor: 'rgba(200,200,200,1)', width: '100%', height: '200px' }}>
+                    {name} Track Item Here
+                </div>
+                :
+                null // Don't Render!
+            }
+        </>
     )
 }
 
