27cf125a267f92960fee9fb591898b2aac66e726
Shrill Shrestha <shrill.shrestha@gmail.com>
2021-12-02 11:48:14 -0600

    Fix carousel opacity error

--- frontend/src/views/dashboard/Carousel.tsx
+++ frontend/src/views/dashboard/Carousel.tsx
@@ -6,8 +6,6 @@ import Slides from "./Links";
 const links = Slides();
 
 const RunCarousel = () => {
-  const opacityHandler = () => {};
-
   const [focused, setFocused] = useState(0);
   const [rotating, setRotating] = useState(false);
   const [testString, setTestString] = useState("(test me here)");
@@ -35,56 +33,52 @@ const RunCarousel = () => {
         index: index,
         focusState: index === focused ? true : false,
         next: index + 1 > links.length - 1 ? 0 : index + 1,
-        opacity: handleOpacity(index),
+        opacity: 1,
       }))
     );
     console.log(...circleQueue);
   }, [focused]);
 
-  const [clickable, setClickable] = useState([
-    -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  ]);
+  // const handleOpacity = (index: any) => {
+  //   const LR3 = 0.5;
+  //   const LR2 = 0.7;
+  //   const LR1 = 0.95;
+  //   const cen = 1;
+  //   let give = 0;
 
-  const handleOpacity = (index: any) => {
-    const LR3 = 0.5;
-    const LR2 = 0.7;
-    const LR1 = 0.95;
-    const cen = 1;
-    let give = 0;
+  //   if (index === focused) {
+  //     give = cen;
+  //   } else if (
+  //     index === focused - 1 ||
+  //     index === focused + 1 ||
+  //     (index === circleQueue.length - 1 && focused === 0) ||
+  //     (index === 0 && focused === circleQueue.length - 1)
+  //   ) {
+  //     give = LR1;
+  //   } else if (
+  //     index === focused - 2 ||
+  //     index === focused + 2 ||
+  //     (index === circleQueue.length - 2 && focused === 0) ||
+  //     (index === circleQueue.length - 1 && focused === 1) ||
+  //     (index === 1 && focused === circleQueue.length - 1) ||
+  //     (index === 0 && focused === circleQueue.length - 2)
+  //   ) {
+  //     give = LR2;
+  //   } else if (
+  //     index === focused - 3 ||
+  //     index === focused + 3 ||
+  //     (index === circleQueue.length - 3 && focused === 0) ||
+  //     (index === circleQueue.length - 2 && focused === 1) ||
+  //     (index === circleQueue.length - 1 && focused === 2) ||
+  //     (index === 2 && focused === circleQueue.length - 1) ||
+  //     (index === 1 && focused === circleQueue.length - 2) ||
+  //     (index === 0 && focused === circleQueue.length - 3)
+  //   ) {
+  //     give = LR3;
+  //   } else give = 0.15;
 
-    if (index === focused) {
-      give = cen;
-    } else if (
-      index === focused - 1 ||
-      index === focused + 1 ||
-      (index === circleQueue.length - 1 && focused === 0) ||
-      (index === 0 && focused === circleQueue.length - 1)
-    ) {
-      give = LR1;
-    } else if (
-      index === focused - 2 ||
-      index === focused + 2 ||
-      (index === circleQueue.length - 2 && focused === 0) ||
-      (index === circleQueue.length - 1 && focused === 1) ||
-      (index === 1 && focused === circleQueue.length - 1) ||
-      (index === 0 && focused === circleQueue.length - 2)
-    ) {
-      give = LR2;
-    } else if (
-      index === focused - 3 ||
-      index === focused + 3 ||
-      (index === circleQueue.length - 3 && focused === 0) ||
-      (index === circleQueue.length - 2 && focused === 1) ||
-      (index === circleQueue.length - 1 && focused === 2) ||
-      (index === 2 && focused === circleQueue.length - 1) ||
-      (index === 1 && focused === circleQueue.length - 2) ||
-      (index === 0 && focused === circleQueue.length - 3)
-    ) {
-      give = LR3;
-    } else give = 0.15;
-
-    return give;
-  };
+  //   return give;
+  // };
 
   // Create an array of Carousel Items
   const items: CarouselItem[] = links.map((item, index) => ({
@@ -92,14 +86,7 @@ const RunCarousel = () => {
     image: "",
     content: (
       <>
-        <div
-          style={{
-            transition: "opacity .3s ease-in-out",
-            opacity: `${circleQueue[index].opacity}`,
-            width: "100%",
-            height: "100%",
-          }}
-        >
+        <div>
           <a href={`${item.link}`} style={{ width: "100%", height: "33%" }}>
             <img src={item.image} style={{ height: "80%" }}></img>
             <div
@@ -109,8 +96,8 @@ const RunCarousel = () => {
                 fontSize: "small",
                 textDecoration: "none",
                 backgroundColor: "black",
-                borderBottomLeftRadius: '10px',
-                borderBottomRightRadius: '10px',
+                borderBottomLeftRadius: "10px",
+                borderBottomRightRadius: "10px",
               }}
             >
               {item.id}
@@ -125,8 +112,8 @@ const RunCarousel = () => {
                 fontSize: "small",
                 textDecoration: "none",
                 backgroundColor: "black",
-                borderBottomLeftRadius: '10px',
-                borderBottomRightRadius: '10px',
+                borderBottomLeftRadius: "10px",
+                borderBottomRightRadius: "10px",
               }}
             >
               {item.id2}
@@ -141,8 +128,8 @@ const RunCarousel = () => {
                 fontSize: "small",
                 textDecoration: "none",
                 backgroundColor: "black",
-                borderBottomLeftRadius: '10px',
-                borderBottomRightRadius: '10px',
+                borderBottomLeftRadius: "10px",
+                borderBottomRightRadius: "10px",
               }}
             >
               {item.id3}
@@ -163,85 +150,48 @@ const RunCarousel = () => {
   };
 
   return (
-    <div className="comp-container">
-      <div>
-        {circleQueue.map((item, index) => {
-          return (
-            <div style={{ background: "green", color: "white", margin: "2px" }}>
-              {item.focusState}
-            </div>
-          );
-        })}
-      </div>
-      <div className="root">
-        <Carousel
-          items={items}
-          itemWidth={200}
-          nextButtonContent={
-            <button
-              style={{
-                position: "relative",
-                // bottom: "20%",
-                // right: "19%",
-                opacity: "1",
-                width: "4rem",
-                height: "4rem",
-                borderRadius: "30%",
-                zIndex: 5,
-              }}
-              onClick={(e: any) => {
-                Promise.all([templatePromise(setRotating, true)])
-                  .then(() => handleClick(1, "next"))
-                  .then(() => setRotating(false));
-              }}
-            >
-              &rarr;
-            </button>
-          }
-          prevButtonContent={
-            <button
-              style={{
-                position: "relative",
-                // top: 0,
-                right: "70%",
-                opacity: "1",
-                width: "4rem",
-                height: "4rem",
-                borderRadius: "30%",
-                zIndex: 5,
-              }}
-              onClick={(e: any) => {
-                Promise.all([templatePromise(setRotating, true)])
-                  .then(() => handleClick(-1, "prev"))
-                  .then(() => setRotating(false));
-              }}
-            >
-              &larr;
-            </button>
-          }
-        />
-      </div>
-      {/* TESTING */}
-      {/* <div
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          background: "white",
-          width: "322px",
-          height: "322px",
-        }}
-      >
-        {rotating ? testString : ""}
-        <div>{`prev: ${circleQueue[focused].prev}`}</div>
-        <div>{`Next: ${circleQueue[focused].next}`}</div>
-        {/* First: 0 <div />
-        Halfway: {Math.floor(circleQueue.length / 2)}
-        <div />
-        Last: {circleQueue.length - 1}
-        <div /> */}
-      {/* </div> */}
-    </div>
+    <Carousel
+      items={items}
+      itemWidth={200}
+      nextButtonContent={
+        <button
+          style={{
+            position: "relative",
+            width: "4rem",
+            height: "4rem",
+            borderRadius: "30%",
+            zIndex: 5,
+          }}
+          onClick={(e: any) => {
+            Promise.all([templatePromise(setRotating, true)])
+              .then(() => handleClick(1, "next"))
+              .then(() => setRotating(false));
+          }}
+        >
+          &rarr;
+        </button>
+      }
+      prevButtonContent={
+        <button
+          style={{
+            position: "relative",
+            // top: 0,
+            right: "70%",
+            width: "4rem",
+            height: "4rem",
+            borderRadius: "30%",
+            zIndex: 5,
+          }}
+          onClick={(e: any) => {
+            Promise.all([templatePromise(setRotating, true)])
+              .then(() => handleClick(-1, "prev"))
+              .then(() => setRotating(false));
+          }}
+        >
+          &larr;
+        </button>
+      }
+    />
   );
 };
 export default RunCarousel;
--- frontend/src/views/dashboard/Dashboard.tsx
+++ frontend/src/views/dashboard/Dashboard.tsx
@@ -1,30 +1,20 @@
-import {
-  Typography,
-  //AppBar,
-  Toolbar,
-  Container,
-} from '@material-ui/core';
-import useStyles from '../../style/DashboardStyles'; // new styling
-import Carousel from './Carousel';
-import MenuAppBar from '../../components/AppBar';
-
+import useStyles from "../../style/DashboardStyles"; // new styling
+import Carousel from "./Carousel";
+import MenuAppBar from "../../components/AppBar";
 
 const Dashboard = () => {
-
-const classes = useStyles();
+  const classes = useStyles();
 
   return (
-    <div className={classes.container} >
-      <div className={classes.background}></div>
-      <div className={classes.page}>
-        <MenuAppBar />
-        <main className={classes.mainAll}>
-          <Container maxWidth="lg" >
-            <Carousel />
-          </Container>
-        </main>
+    <>
+      <div className={classes.container}>
+        <div className={classes.background}></div>
+        <div className={classes.page}>
+          <MenuAppBar />
+          <Carousel />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
